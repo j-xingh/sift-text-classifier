ffd04095c7d27c1706a37e8262e9e093b8ffc134
j-xingh <jay.singh4teen@gmail.com>
2026-09-15 01:28:47 +0530

    fix: allow production frontend in CORS

--- api/main.py
+++ api/main.py
@@ -37,8 +37,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://frontend-six-lilac-69.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
